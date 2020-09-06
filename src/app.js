657ea0bc57d30b0fc75cc392fcbb6a3e78ae8f18
Ysom <1083688285@qq.com>
2020-09-06 16:42:06 +0800

    index.html增加layout布局展示

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import Button from './components/Button'
 import ButtonGroup from './components/ButtonGroup'
+import Row from './components/Row'
+import Col from './components/Col'
 
 Vue.component('YvButton', Button);
 Vue.component('YvButtonGroup', ButtonGroup)
+Vue.component('YvRow', Row)
+Vue.component('YvCol', Col)
 
 new Vue({
   el: '#app',
