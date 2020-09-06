a96d32827015a098af49f84ee0167f76cbd38015
Ysom <1083688285@qq.com>
2020-09-06 16:34:20 +0800

    增加row组件

---
+++ src/components/Row.vue
@@ -0,0 +1,57 @@
+<template>
+  <div
+    class="yv-row"
+    :class="rowClass"
+    :style="rowStyle"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "YvRow",
+    props: {
+      gutter: [String, Number],
+      margin: {
+        type: String,
+        validator(value) {
+          return ['left', 'center', 'right'].includes(value)
+        }
+      }
+    },
+    computed: {
+      rowClass() {
+        const { align } = this
+        return [align && `align-${align}`]
+      },
+      rowStyle() {
+        return {
+          marginLeft: -this.gutter/2 + 'px',
+          marginRight: -this.gutter/2 + 'px'
+        }
+      }
+    },
+    mounted() {
+      this.$children.forEach(child => {
+        child.gutter = this.gutter
+      })
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+.yv-row {
+  display: flex;
+  flex-wrap: wrap;
+  &.align-left {
+    justify-content: flex-start;
+  }
+  &.align-center {
+    justify-content: center;
+  }
+  &.align-right {
+    justify-content: flex-end;
+  }
+}
+</style>
