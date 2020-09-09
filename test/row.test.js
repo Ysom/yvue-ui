867204e4dbdb6773e9eb749ba0cc46d1c0c9df22
Ysom <1083688285@qq.com>
2020-09-09 21:41:03 +0800

    增加row测试

---
+++ test/row.test.js
@@ -0,0 +1,39 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Row from '../src/components/Row'
+import Col from '../src/components/Col'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Row', () => {
+  it('Row存在 ', function () {
+    expect(Row).to.be.ok
+  })
+  it('可以接收gutter ', function (done) {
+    Vue.component('yv-row', Row)
+    Vue.component('yv-col', Col)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+      <yv-row :gutter="10">
+        <yv-col :span="12"></yv-col>
+        <yv-col :span="12"></yv-col>
+      </yv-row>
+    `
+    const vm = new Vue({
+      el: 'div'
+    })
+    setTimeout(() => {
+      const row = vm.$el.querySelector('.yv-row')
+      expect(getComputedStyle(row).marginLeft).to.be.eq('-5px')
+      expect(getComputedStyle(row).marginRight).to.be.eq('-5px')
+      const col = document.querySelectorAll('.yv-col')
+      expect(getComputedStyle(col[0]).paddingLeft).to.be.eq('5px')
+      expect(getComputedStyle(col[1]).paddingRight).to.be.eq('5px')
+      done()
+      vm.$el.remove()
+      vm.$destroy()
+    })
+  })
+})
