11885fe9002f9b1899f64712a6e897872894cf58
Ysom <1083688285@qq.com>
2020-09-12 22:31:48 +0800

    增加Footer组件

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="yv-footer">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "YvFooter"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
