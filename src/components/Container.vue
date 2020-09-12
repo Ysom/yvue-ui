b0ac9f0db7bcdae3814fb496feb75ab8d2b61233
Ysom <1083688285@qq.com>
2020-09-12 22:32:09 +0800

    增加Container组件

---
+++ src/components/Container.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="yv-container" :class="containerClass">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "YvContainer",
+  data() {
+    return {
+      containerClass: {
+        hasAside: false
+      }
+    }
+  },
+  mounted() {
+    this.$children.some(child => {
+      if (child.$options.name === 'YvAside') {
+        this.containerClass.hasAside = true
+        return true
+      }
+    })
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.yv-container {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  &.hasAside {
+    flex-direction: row;
+  }
+}
+</style>
