45afbb79642257c845607232c4410772c4263fda
Ysom <1083688285@qq.com>
2020-09-12 22:31:32 +0800

    增加Aside组件

---
+++ src/components/Aside.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="yv-aside">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "YvAside",
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
