2729278909e5ddf1015dc3dd8f423ca1f62b9bac
Ysom <1083688285@qq.com>
2020-09-12 22:31:17 +0800

    增加Main组件

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="yv-main">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "YvMain",
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.yv-main {
+  flex-grow: 1;
+}
+</style>
