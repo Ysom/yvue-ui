c3ba163fc5f6b8466b3bd4e30860dcab28306606
Ysom <1083688285@qq.com>
2020-09-12 22:30:59 +0800

    增加Header组件

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="yv-header">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "YvHeader",
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
