faa14b847ec1881881a2701b18599d48934e236d
Ysom <1083688285@qq.com>
2020-09-13 17:48:24 +0800

    index.html增加container示例

--- src/app.js
+++ src/app.js
@@ -3,11 +3,21 @@ import Button from './components/Button'
 import ButtonGroup from './components/ButtonGroup'
 import Row from './components/Row'
 import Col from './components/Col'
+import Container from './components/Container'
+import Header from './components/Header'
+import Aside from './components/Aside'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 Vue.component('YvButton', Button);
 Vue.component('YvButtonGroup', ButtonGroup)
 Vue.component('YvRow', Row)
 Vue.component('YvCol', Col)
+Vue.component('YvContainer', Container)
+Vue.component('YvHeader', Header)
+Vue.component('YvAside', Aside)
+Vue.component('YvMain', Main)
+Vue.component('YvFooter', Footer)
 
 new Vue({
   el: '#app',
