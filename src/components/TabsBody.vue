a9da2719cb944c5971b84ede5e6dbf7cfc7bba91
Ysom <1083688285@qq.com>
2020-09-18 23:29:36 +0800

    增加TabsBody组件

---
+++ src/components/TabsBody.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="yv-tabs-body">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "YvTabsBody",
+  inject:['eventBus'],
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
