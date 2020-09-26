5f830afc5d2b9b4185c349adcf8aa0d08f0a3af3
Ysom <1083688285@qq.com>
2020-09-26 17:40:37 +0800

    将Toast处理成插件

---
+++ src/components/plugin.js
@@ -0,0 +1,30 @@
+import Toast from './Toast'
+
+let currentToast
+
+export default {
+  install(Vue, options) {
+    Vue.prototype.$toast = function(toastOptions) {
+      if (currentToast) {
+        currentToast.onClose()
+      }
+      currentToast = createToast({
+        Vue,
+        propsData: toastOptions,
+        onClose: () => {
+          currentToast = null
+        }
+      })
+    }
+  }
+}
+
+function createToast({ Vue, propsData, onClose }) {
+  const Constructor = Vue.extend(Toast)
+  const toast = new Constructor({ propsData })
+  toast.$slots.default = [propsData.message]
+  toast.$mount()
+  toast.$on('close', onClose)
+  document.body.appendChild(toast.$el)
+  return toast
+}
