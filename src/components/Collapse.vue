ee9a9cf2c12c6a523f23ac8882032807bc593ff1
Ysom <1083688285@qq.com>
2020-10-13 23:01:57 +0800

    增加collapse、collapseItem组件

---
+++ src/components/Collapse.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class='yv-collapse'>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default {
+  name: 'YvCollapse',
+  props: {
+    single: {
+      type: Boolean,
+      default: false
+    },
+    selected: {
+      type: Array
+    }
+  },
+  provide() {
+    return {
+      eventBus: this.eventBus
+    }
+  },
+  data() {
+    return {
+      eventBus: new Vue(),
+      selectedArray: []
+    }
+  },
+  mounted() {
+    this.eventBus.$emit('update:selected', this.selected)
+    this.eventBus.$on('update:addSelected', (name) => {
+      this.selectedArray = JSON.parse(JSON.stringify(this.selected))
+      if (this.single) {
+        this.selectedArray = [name]
+      } else {
+        this.selectedArray.push(name)
+      }
+      this.eventBus.$emit('update:selected', this.selectedArray)
+      this.$emit('update:selected', this.selectedArray)
+    })
+    this.eventBus.$on('update:removeSelected', (name) => {
+      this.selectedArray = JSON.parse(JSON.stringify(this.selected))
+      let index = this.selectedArray.indexOf(name)
+      this.selectedArray.splice(index, 1)
+      this.eventBus.$emit('update:selected', this.selectedArray)
+      this.$emit('update:selected', this.selectedArray)
+    })
+  }
+}
+</script>
+
+<style scoped lang="scss">
+$border-color: #999;
+.yv-collapse {
+  border-radius: 4px;
+  border: 1px solid $border-color;
+}
+</style>
---
+++ src/components/CollapseItem.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class='yv-collapse-item' @click="toggle">
+    <div class="title">
+      {{ title }}
+    </div>
+    <div class="content" v-if="open">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'YvCollapseItem',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String
+    }
+  },
+  inject: ['eventBus'],
+  data() {
+    return {
+      open: false,
+    }
+  },
+  mounted() {
+    this.eventBus && this.eventBus.$on('update:selected', (names) => {
+      this.open = names.indexOf(this.name) >= 0
+    })
+  },
+  methods: {
+    toggle() {
+      if (this.open) {
+        this.eventBus && this.eventBus.$emit('update:removeSelected', this.name)
+      } else {
+        this.eventBus && this.eventBus.$emit('update:addSelected', this.name)
+      }
+    },
+  }
+}
+</script>
+
+<style scoped lang="scss">
+$grey: #999;
+$border-radius: 4px;
+.yv-collapse-item {
+  .title {
+    margin: -1px;
+    padding: 0 8px;
+    border: 1px solid $grey;
+    background-color: rgb(245, 245, 245);
+    display: flex;
+    align-items: center;
+    min-height: 32px;
+  }
+
+  &:first-child {
+    .title {
+      border-top-left-radius: $border-radius;
+      border-top-right-radius: $border-radius;
+    }
+  }
+
+  &:last-child {
+    .title:last-child {
+      border-bottom-left-radius: $border-radius;
+      border-bottom-right-radius: $border-radius;
+    }
+  }
+
+  .content {
+    padding: 8px;
+  }
+}
+</style>
