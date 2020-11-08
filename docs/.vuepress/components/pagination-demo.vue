0d0426333f122747a1b7a2a154040b8d8d85fe98
Ysom <1083688285@qq.com>
2020-11-08 15:23:19 +0800

    增加分页pagination示例

---
+++ docs/.vuepress/components/pagination-demo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="pagination-demo-con">
+    <yv-pagination
+      :total-page="totalPage"
+      :current-page="currentPage"
+      @change="onPageChange"
+    ></yv-pagination>
+  </div>
+</template>
+
+<script>
+import Pagination from '../../../src/components/Pagination'
+
+export default {
+  name: "pagination-demo",
+  components: {
+    YvPagination: Pagination
+  },
+  data() {
+    return {
+      currentPage: 1,
+      totalPage: 10
+    }
+  },
+  methods: {
+    onPageChange(page) {
+      this.currentPage = page
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.pagination-demo-con {
+  margin-top: 20px;
+}
+</style>
