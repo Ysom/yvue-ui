3f355fd555c37f8c11592cc7f73606db2400b2c3
Ysom <1083688285@qq.com>
2021-02-21 12:50:45 +0800

    修改配置，发布到npm

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
 import YvButton from './src/components/button/Button'
+import YvButtonGroup from './src/components/button/ButtonGroup'
+import YvCarousel from './src/components/carousel/Carousel'
+import YvCarouselItem from './src/components/carousel/CarouselItem'
+import YvCollapse from './src/components/collapse/Collapse'
+import YvCollapseItem from './src/components/collapse/CollapseItem'
+import YvContainer from './src/components/container/Container'
+import YvAside from './src/components/container/Aside'
+import YvHeader from './src/components/container/Header'
+import YvMain from './src/components/container/Main'
+import YvFooter from './src/components/container/Footer'
+import YvIcon from './src/components/icon/Icon'
+import YvCol from './src/components/layout/Col'
+import YvRow from './src/components/layout/Row'
+import YvPagination from './src/components/pagination/Pagination'
+import YvPopover from './src/components/popover/Popover'
+import YvTabs from './src/components/tabs/Tabs'
+import YvTabsItem from './src/components/tabs/TabsItem'
+import YvTabsHead from './src/components/tabs/TabsHead'
+import YvTabsBody from './src/components/tabs/TabsBody'
+import YvTabsPane from './src/components/tabs/TabsPane'
+import YvToast from './src/components/toast/Toast'
+import YvPlugins from './src/components/plugin'
 
-export { YvButton }
+export {
+  YvButton,
+  YvButtonGroup,
+  YvCarousel,
+  YvCarouselItem,
+  YvAside,
+  YvCollapse,
+  YvCol,
+  YvCollapseItem,
+  YvContainer,
+  YvFooter,
+  YvHeader,
+  YvIcon,
+  YvMain,
+  YvPagination,
+  YvPlugins,
+  YvPopover,
+  YvRow,
+  YvTabs,
+  YvTabsBody,
+  YvTabsHead,
+  YvTabsItem,
+  YvTabsPane,
+  YvToast
+}
